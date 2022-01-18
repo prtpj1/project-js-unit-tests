85d41c2ce7f28b2652d558dc9f7199f34a49d41d
Paulo Porto <prtpj1@gmail.com>
2022-01-18 18:22:34 -0300

    removed "for" and add same function type as rest of code

--- src/average.js
+++ src/average.js
@@ -17,22 +17,24 @@ const average = (notas) => {
 let notasValues = Object.values(notas);
 let notasKeys = Object.keys(notas).length;
 
-for (let i = 0; i < notas.length; i += 1) {
-if (typeof notas[i] !== 'number') {
-return undefined;
-}
-}
-if (notasValues.some((check) => check === '')) {
-return undefined;
-}
-if (notasValues.every((check) => check === [])) {
-return undefined;
-}
-let notasSoma = notas.reduce(function (total, num) {
-return total + num;
-});
-let result = Math.round(notasSoma / notasKeys);
-return result;
+  if (notasValues.some((check) => typeof check !== 'number')) {
+    return undefined;
+  }
+
+  if (notasValues.some((check) => check === '')) {
+    return undefined;
+  }
+
+  if (notasValues.every((check) => check === [])) {
+    return undefined;
+  }
+
+  let notasSoma = notas.reduce(function (total, num) {
+    return total + num;
+  });
+  
+  let result = Math.round(notasSoma / notasKeys);
+    return result;
 };
 
 module.exports = average;
