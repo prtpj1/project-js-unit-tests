050e193e50550bfee45655b6cec92d4ebc639b35
Paulo Porto <prtpj1@gmail.com>
2022-01-19 19:14:06 -0300

    Req 4 - Done

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,33 +25,41 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
+    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
     expect(circle(isNaN)).toBe(undefined);
+
+    // Teste se circle retorna um objeto.
     expect(circle(1).area).toBeCloseTo(3.14);
+
+    // Teste se o objeto retornado tem 3 propriedades.
     expect(circle(1)).toMatchObject({
       radius: 1,
       area: 3.14,
       circumference: 6.28,
     });
-    expect(circle()).toBeUndefined();
-    expect(circle(2)).toBeCloseTo({ circumference: 12.56 });
-    expect(circle(3)).toBeCloseTo({ area: 28.26 });
-    expect(circle(3)).toBeCloseTo({
-      radius: 3,
-      area: 28.26,
-      circumference: 18.84,
-    });
-
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 propriedades.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
+
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
+    expect(circle(2).circumference).toBeCloseTo(12.56);
+
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    expect(circle(3).area).toBeCloseTo(28.26);
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3).radius).toBeCloseTo(3);
+    expect(circle(3).area).toBeCloseTo(28.26);
+    expect(circle(3).circumference).toBeCloseTo(18.84);
   });
 });
-
+//expect(circle(1)).toEqual([1, 3.14, 6.28]);
 // expect(circle(1)).toEqual({radius: 1, area: 3.14, circumference: 6.28});
 // expect(circle(1)).toBeCloseTo(1, 3.14, 6.28);
 // expect(circle(7)).toBeCloseTo(7, 153.86, 43.96);
 // expect(circle(3)).toBeCloseTo(3, 28.26, 18.84);
+/* expect(circle(3)).toBeCloseTo({
+  radius: 3,
+  area: 28.26,
+  circumference: 18.84,
+}); */
